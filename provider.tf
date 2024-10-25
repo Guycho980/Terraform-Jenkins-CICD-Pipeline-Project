a42adc74bd1af85f892bd89aee5fe96eac79a2ac
Guycho980 <nyguycho@gmail.com>
2024-10-24 20:38:15 -0400

    updated config files

--- provider.tf
+++ provider.tf
@@ -14,7 +14,7 @@ terraform {
 # Terraform Provider Block
 provider "google" {
   # Configuratio options
-  project     = "test-environment-te-project"
+  project     = "trusty-sol-433013-q7"
   region      = "us-central1"
   zone        = "us-central1-a"
 }
